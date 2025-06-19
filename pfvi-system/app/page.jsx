3f6a1816ac3269d98a36266bf3b3a3db76b66455
Pribbled <joshua_domanais@dlsu.edu.ph>
2025-06-19 10:40:00 +0800

    Revert "Fixed the homepage redirection based on the session"
    This reverts commit 6e244e984bbe7b097fcb87e4baf5ef99cb7016e2.

--- pfvi-system/app/page.jsx
+++ pfvi-system/app/page.jsx
@@ -2,24 +2,11 @@ import LoginPage from "./login/page";
 import HomeSecretary from './secretary/page';
 import HomeDriver from './driver/page';
 import HomeSalesman from './salesman/page';
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return <LoginPage />
-  }
-
-  switch (session.user.role) {
-    case "secretary":
-      return <HomeSecretary />;
-    case "driver":
-      return <HomeDriver />;
-    case "salesman":
-      return <HomeSalesman />;
-    default:
-      return <LoginPage />;
-  }
+  return (
+    <main>
+      <HomeSecretary />
+    </main>
+  );
 }
