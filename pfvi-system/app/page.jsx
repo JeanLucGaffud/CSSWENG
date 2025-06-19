40c743e5438a1f8f75ffa83c53eb208482ba53dd
Pribbled <joshua_domanais@dlsu.edu.ph>
2025-06-19 10:41:46 +0800

    Fixed the homepage redirection based on the session

--- pfvi-system/app/page.jsx
+++ pfvi-system/app/page.jsx
@@ -2,11 +2,24 @@ import LoginPage from "./login/page";
 import HomeSecretary from './secretary/page';
 import HomeDriver from './driver/page';
 import HomeSalesman from './salesman/page';
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export default async function Home() {
-  return (
-    <main>
-      <HomeSecretary />
-    </main>
-  );
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return <LoginPage />
+  }
+
+  switch (session.user.role) {
+    case "secretary":
+      return <HomeSecretary />;
+    case "driver":
+      return <HomeDriver />;
+    case "salesman":
+      return <HomeSalesman />;
+    default:
+      return <LoginPage />;
+  }
 }
